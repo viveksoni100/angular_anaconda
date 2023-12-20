1da04dd744d6a34722af742b275728661739fccb
viveksoni100 <viveksoni100@gmail.com>
2023-12-20 12:45:14 +0530

    ngModel learned

---
+++ my-first-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Vivek Soni';
+  name = "";
+  // title = 'my-first-app';
+}
