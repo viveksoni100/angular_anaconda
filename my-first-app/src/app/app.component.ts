1e6028045e08793b02d1f6ccc8b3f9dd5376c6a0
viveksoni100 <viveksoni100@gmail.com>
2023-12-20 13:56:44 +0530

    clean is set to clean

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,17 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Vivek Soni';
-  name = "";
-  // title = 'my-first-app';
+
 }
