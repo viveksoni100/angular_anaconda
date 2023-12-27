890804e66ce15158f0a2afa94f87c6b4ee87d21d
viveksoni100 <viveksoni100@gmail.com>
2023-12-27 15:30:48 +0530

    practicles are done

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -14,4 +14,18 @@ import {CommonModule} from "@angular/common";
 })
 export class AppComponent {
   servers = ['UAT_Server','PROD_Server', 'DEV_Server'];
+  para = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  paraToDisplay = false;
+  logClicks = [];
+
+  toggleDisplayOptions() {
+    // console.log(this.logClicks);
+    if (this.paraToDisplay == false) {
+      this.paraToDisplay = true;
+      this.logClicks.push('clicked');
+    } else {
+      this.paraToDisplay = false;
+      this.logClicks.push('clicked');
+    }
+  }
 }
