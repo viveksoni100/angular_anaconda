e2036c6e247ca0e3c09813d6258f3a12c66549ff
viveksoni100 <viveksoni100@gmail.com>
2023-12-27 12:33:56 +0530

    ngFor directive learned

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,14 +3,15 @@ import {ServerComponent} from "./server/server.component";
 import {ServersComponent} from "./servers/servers.component";
 import {SuccessAlertComponent} from "./success-alert/success-alert.component";
 import {WarningAlertComponent} from "./warning-alert/warning-alert.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ServerComponent, ServersComponent, SuccessAlertComponent, WarningAlertComponent],
+  imports: [ServerComponent, ServersComponent, SuccessAlertComponent, WarningAlertComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  servers = ['UAT_Server','PROD_Server', 'DEV_Server'];
 }
