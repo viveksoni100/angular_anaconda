40db059cfa9eae28d2f2da0c795da029f51087f5
viveksoni100 <viveksoni100@gmail.com>
2023-12-28 20:00:15 +0530

    shopping form initiated

--- my-bistro/src/app/shopping-list/shopping-list.component.ts
+++ my-bistro/src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
+import {Ingredient} from "../shared/ingredient.model";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [ShoppingEditComponent],
+  imports: [ShoppingEditComponent, CommonModule],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 }
