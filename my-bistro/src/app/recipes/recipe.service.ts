cbfe4c7a6f685d2ce5c9a82716a2ddee7333751d
viveksoni100 <viveksoni100@gmail.com>
2023-12-29 20:46:56 +0530

    recipe service added

--- my-bistro/src/app/recipes/recipe.service.ts
+++ my-bistro/src/app/recipes/recipe.service.ts
@@ -1,3 +1,18 @@
+import {Recipe} from "./recipe.model";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root',
+})
 export class RecipeService {
 
+  private recipes: Recipe[] = [
+    new Recipe('Samosa','Paneer Samosa', 'https://media.istockphoto.com/id/502663991/photo/punjabi-samosa-23.jpg?s=612x612&w=0&k=20&c=Ne0ArOpa-4L2N-INtPnq371fFZoM6qWW6R4ryN9TWW0='),
+    new Recipe('Vadapav','Bombay Vadapav', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5LwQ4cgMRH-elYUgCunOYaEae1znx7LVTZmIv_tdQiw&s'),
+    new Recipe('Springroll','Cheese Springroll', 'https://www.nestleprofessional.in/sites/default/files/2022-05/Vegetable-Spring-Roll_0.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
--- my-bistro/src/app/recipes/recipes.component.ts
+++ my-bistro/src/app/recipes/recipes.component.ts
@@ -3,6 +3,7 @@ import {RecipesListComponent} from "./recipes-list/recipes-list.component";
 import {RecipesDetailComponent} from "./recipes-detail/recipes-detail.component";
 import {Recipe} from "./recipe.model";
 import {CommonModule} from "@angular/common";
+import {RecipeService} from "./recipe.service";
 
 @Component({
   selector: 'app-recipes',
@@ -13,4 +14,7 @@ import {CommonModule} from "@angular/common";
 })
 export class RecipesComponent {
   selectedRecipe!: Recipe;
+
+  constructor(private recipeService: RecipeService) {
+  }
 }
