08d07c17a47f70d12e5ce156f43bb418b7aeb21e
viveksoni100 <viveksoni100@gmail.com>
2023-12-29 12:53:57 +0530

    recipe-item and recipe-list component interconnected

--- my-bistro/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ my-bistro/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Recipe} from "../../recipe.model";
 import {CommonModule} from "@angular/common";
 
@@ -10,8 +10,6 @@ import {CommonModule} from "@angular/common";
   styleUrl: './recipes-item.component.css'
 })
 export class RecipesItemComponent {
-  recipes: Recipe[] = [
-    new Recipe('Samosa','Paneer Samosa', 'https://media.istockphoto.com/id/502663991/photo/punjabi-samosa-23.jpg?s=612x612&w=0&k=20&c=Ne0ArOpa-4L2N-INtPnq371fFZoM6qWW6R4ryN9TWW0='),
-    new Recipe('Vadapav','Bombay Vadapav', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5LwQ4cgMRH-elYUgCunOYaEae1znx7LVTZmIv_tdQiw&s')
-  ];
+  @Input() recipe!: Recipe;
+
 }
