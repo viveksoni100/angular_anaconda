08008b333c6058aef2503b0bd8ce32492a313c5a
viveksoni100 <viveksoni100@gmail.com>
2024-01-01 20:58:32 +0530

    app-routing.module.ts added

--- my-bistro/src/app/app.module.ts
+++ my-bistro/src/app/app.module.ts
@@ -8,11 +8,21 @@ import {RecipesDetailComponent} from "./recipes/recipes-detail/recipes-detail.co
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
 import {DropdownDirective} from "./shared/dropdown.directive";
+import {AppRoutingModule} from "./app-routing.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent, HeaderComponent, RecipesComponent, RecipesListComponent, RecipesItemComponent,
     RecipesDetailComponent, ShoppingListComponent, ShoppingEditComponent, DropdownDirective,     RecipesDetailComponent, ShoppingListComponent, ShoppingEditComponent, DropdownDirective
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ]
 })
 export class AppModule {
