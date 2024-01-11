6dbc0cfafb9de7d3e27ff5b659eec31e6c3e7e17
viveksoni100 <viveksoni100@gmail.com>
2024-01-12 00:20:17 +0530

    observable learning started

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription, Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription!: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    /*this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });*/
+
+    let customIntervalObservable = Observable.create((observer) => {
+      setInterval(() => {
+        observer.next();
+      }, 1000);
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    /*this.firstObsSubscription.unsubscribe();*/
   }
 
 }
